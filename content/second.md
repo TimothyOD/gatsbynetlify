9ff7edf22397eaf4d9c2c168e5a2728597d57e01
Tim <Tim@192.168.1.7>
2020-06-04 16:20:27 +0700

    second

---
+++ content/second.md
@@ -0,0 +1,5 @@
+---
+title: Second
+intro: Second
+image: assets/habbo_2020-05-21_00-56-10.png
+---
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -2,15 +2,41 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
+import Image from "../components/image"
 import SEO from "../components/seo"
+import { graphql } from "gatsby"
 
-const SecondPage = () => (
-  <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+const IndexPage = props => {
+  const data = props.data.allFile.edges[0].node.childMarkdownRemark.frontmatter
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1> {data.title} </h1> <p> {data.intro} </p>{" "}
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <img src={data.image} alt="Habbo!" />
+      </div>{" "}
+      <Link to="/page-2/"> Go to page 2 </Link>{" "}
+    </Layout>
+  )
+}
 
-export default SecondPage
+export default IndexPage
+export const query = graphql`
+  query {
+    allFile(
+      filter: { sourceInstanceName: { eq: "content" }, name: { eq: "home" } }
+    ) {
+      edges {
+        node {
+          childMarkdownRemark {
+            frontmatter {
+              title
+              intro
+              image
+            }
+          }
+        }
+      }
+    }
+  }
+`
